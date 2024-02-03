2c92ce39bd451ed5826fa410e4683e6f1b30645e
Cheyrekov <zahari.cheyreckov999@gmail.com>
2024-02-03 13:54:45 +0200

    Extract console.log

--- middleware/src/app/api/hello/route.ts
+++ middleware/src/app/api/hello/route.ts
@@ -3,9 +3,7 @@ import { limiter } from "../config/limiter";
 
 export async function GET(request: Request) {
   const origin = request.headers.get("origin");
-
   const remaining = await limiter.removeTokens(1);
-  console.log("remaining:", remaining);
 
   if (remaining < 0) {
     return new NextResponse(null, {

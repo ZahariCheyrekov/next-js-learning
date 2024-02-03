5590c6e7cb891916f0f14d5cd1896822427410a9
Cheyrekov <zahari.cheyreckov999@gmail.com>
2024-02-03 12:10:25 +0200

    Build routes

---
+++ route-handlers/src/app/api/echo/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+// export async function GET(request: Request) {
+//   const { searchParams } = new URL(request.url);
+//   const name = searchParams.get("name");
+//   const instrument = searchParams.get("instrument");
+
+//   return NextResponse.json({ name, instrument });
+// }
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+
+  const obj = Object.fromEntries(searchParams.entries());
+
+  return NextResponse.json(obj);
+}

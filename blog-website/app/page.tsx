1601f5e765fef31d28d3a3d5f683cb3193c4bd2b
Cheyrekov <zahari.cheyreckov999@gmail.com>
2024-02-03 09:49:04 +0200

    Initial commit

---
+++ blog-website/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <p>Hello and Welcome</p>
+      <span>
+        I'm <span className="font-bold">Zahari</span>.
+      </span>
+    </main>
+  );
+}
